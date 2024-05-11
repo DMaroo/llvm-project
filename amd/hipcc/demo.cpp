74a796f292d79533c50760a77e20627cf7423440
Dhruv Maroo <dhruvmaru007@gmail.com>
2024-05-11 19:26:01 +0530

    First demo

--- amd/hipcc/demo.cpp
+++ amd/hipcc/demo.cpp
@@ -22,9 +22,9 @@ THE SOFTWARE.
 
 #include <hip/hip_runtime.h>
 #include <iostream>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 #define SAMPLE_VERSION "HIP-Examples-Application-v1.0"
 #define SUCCESS 0
@@ -33,15 +33,15 @@ THE SOFTWARE.
 using namespace std;
 
 typedef struct access_t {
-	uint64_t thread;
-	uint64_t access_time;
+  uint64_t thread;
+  uint64_t access_time;
+  bool is_write;
 } Access;
 
-constexpr uint64_t ACCESS_SIZE = 256;
+constexpr uint64_t ACCESS_SIZE = (1 << 16);
 
 __device__ Access accesses[ACCESS_SIZE];
 __device__ uint64_t global_clock;
-__device__ uint64_t access_count;
 __device__ uint32_t write_lock;
 __device__ bool race;
 
@@ -49,30 +49,67 @@ extern "C" __device__ void __tsan_func_entry(void *pc) {
   global_clock = 1;
   printf("Entered a function\n");
   race = false;
-	write_lock = 0;
+  write_lock = 0;
 }
 
 extern "C" __device__ void __tsan_write4(void *mem) {
-	bool leaveLoop = false;
-	    while (!leaveLoop) {
-		            if (atomicExch(&write_lock, 1) == 0) {
-				                //critical section
-  printf("Writing 4 bytes\n");
-  if (accesses.access_time == global_clock && accesses.thread != threadIdx.x) {
-	 race = true; 
+  bool leaveLoop = false;
+  uint64_t access_index = (((uint64_t) mem >> 2) & (ACCESS_SIZE - 1));
+  uint64_t index = blockDim.x * blockIdx.x + threadIdx.x;
+  while (!leaveLoop) {
+    if (atomicExch(&write_lock, 1) == 0) {
+      // critical section
+      printf("Writing 4 bytes\n");
+      if (accesses[access_index].access_time == global_clock &&
+          accesses[access_index].thread != index) {
+        race = true;
+        const char *op;
+        if (accesses[access_index].is_write) {
+          op = "Written";
+        } else {
+          op = "Read";
+        }
+        uint64_t tidx = threadIdx.x;
+        uint64_t bidx = blockIdx.x;
+        printf("Data race at %p: \t\t%s by (%lu, %lu), \t\tWritten by (%lu, %lu)\n", mem, op, accesses[access_index].thread / blockDim.x, accesses[access_index].thread % blockDim.x, bidx, tidx);
+      }
+      accesses[access_index].access_time = global_clock;
+      accesses[access_index].thread = index;
+      accesses[access_index].is_write = true;
+      leaveLoop = true;
+      atomicExch(&write_lock, 0);
+    }
   }
-  accesses.access_time = global_clock;
-  accesses.thread = threadIdx.x;
-  leaveLoop = true;
-  atomicExch(&write_lock, 0);
-			    } 
-	    }
+}
+
+extern "C" __device__ void __tsan_read4(void *mem) {
+  bool leaveLoop = false;
+  uint64_t access_index = (((uint64_t) mem >> 2) & (ACCESS_SIZE - 1));
+  uint64_t index = blockDim.x * blockIdx.x + threadIdx.x;
+  while (!leaveLoop) {
+    if (atomicExch(&write_lock, 1) == 0) {
+      // critical section
+      if (accesses[access_index].access_time == global_clock &&
+          accesses[access_index].thread != index &&
+          accesses[access_index].is_write) {
+        race = true;
+        uint64_t tidx = threadIdx.x;
+        uint64_t bidx = blockIdx.x;
+        printf("Data race at %p: \t\tWritten by (%lu, %lu), \t\tRead by (%lu, %lu)\n", mem, accesses[access_index].thread / blockDim.x, accesses[access_index].thread % blockDim.x, bidx, tidx);
+      }
+      accesses[access_index].access_time = global_clock;
+      accesses[access_index].thread = index;
+      accesses[access_index].is_write = false;
+      leaveLoop = true;
+      atomicExch(&write_lock, 0);
+    }
+  }
 }
 
 extern "C" __device__ void __tsan_func_exit() {
   printf("Exited a function\n");
   if (race) {
-	  printf("Data race detected!\n");
+    printf("Data race detected!\n");
   }
 }
 
@@ -84,7 +121,7 @@ extern "C" __device__ void __tsan_func_exit() {
 
 // __device__ void __tsan_func_exit() { return; }
 
-__global__ void helloworld(int *a) { if (threadIdx.x % 8 == 7) { *a = 42; } }
+__global__ void helloworld(int *a) { *a += 5; }
 
 #define HIP_CHECK(condition)                                                   \
   do {                                                                         \
@@ -110,10 +147,10 @@ int main(int argc, char *argv[]) {
   HIP_CHECK(hipMalloc(&d_inp, sizeof(int)));
   HIP_CHECK(hipMemcpy(d_inp, input, sizeof(int), hipMemcpyHostToDevice));
 
-//  int *d_accesses;
-//  HIP_CHECK(hipMalloc(&d_accesses, sizeof(Access)));
-//  HIP_CHECK(hipMemset(d_accesses, 0, sizeof(Access)));
-//  HIP_CHECK(hipMemcpyToSymbol("accesses", &d_accesses, sizeof(Access *)));
+  //  int *d_accesses;
+  //  HIP_CHECK(hipMalloc(&d_accesses, sizeof(Access)));
+  //  HIP_CHECK(hipMemset(d_accesses, 0, sizeof(Access)));
+  //  HIP_CHECK(hipMemcpyToSymbol("accesses", &d_accesses, sizeof(Access *)));
 
   // HIP_CHECK(hipMemcpyToSymbol(HIP_SYMBOL(gg), some_global, 5 * sizeof(int)));
   helloworld<<<dim3(1), dim3(18), 0, 0>>>(d_inp);
@@ -124,4 +161,3 @@ int main(int argc, char *argv[]) {
   std::cout << "Passed!\n";
   return SUCCESS;
 }
-
